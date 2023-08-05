ad4a90ab9d08f805596e7741bc0733dec0952448
CML120 <cheelor922@gmail.com>
2023-08-05 15:11:29 -0500

    added comments to the games and speech codes

--- client/src/SpeechRecognitionComponent.js
+++ client/src/SpeechRecognitionComponent.js
@@ -1,27 +1,40 @@
+//Import dependencies
 import React, { useState, useEffect } from 'react';
 import annyang from 'annyang';
 
+// SpeechRecognitionComponent to handle speech recognition
 const SpeechRecognitionComponent = ({ handleSpokenWord }) => {
+  // State variable to keep track of the listening status
   const [listening, setListening] = useState(false);
 
+  // useEffect hook to initialize and configure annyang when the component mounts
   useEffect(() => {
+    // Check if annyang is available
     if (annyang) {
+      // Define speech recognition commands
       const commands = {
-        '*word': (word) => handleSpokenWord(word),
+        '*word': (word) => handleSpokenWord(word), // When any word is recognized, call handleSpokenWord function with the recognized word.
       };
+      
+      // Add the defined commands to annyang
       annyang.addCommands(commands);
     }
-  }, [handleSpokenWord]);
+  }, [handleSpokenWord]); // The effect depends on the handleSpokenWord function, so it will re-run if it changes.
 
+  // Function to toggle the listening status
   const toggleListening = () => {
     if (listening) {
+      // If listening, stop the speech recognition
       annyang.abort();
     } else {
+      // If not listening, start the speech recognition
       annyang.start();
     }
+    // Toggle the listening state
     setListening(!listening);
   };
 
+  // JSX content for the SpeechRecognitionComponent
   return (
     <div>
       <button onClick={toggleListening}>
--- client/src/SpellingGame.js
+++ client/src/SpellingGame.js
@@ -1,37 +1,46 @@
+//Import dependencies
 import React, { useState, useEffect } from 'react';
 import SpeechRecognitionComponent from './SpeechRecognitionComponent';
 import './SpellingGame.css';
 
 const SpellingGame = () => {
+  // Variables to keep track of the spoken and correct words
   const [spokenWord, setSpokenWord] = useState('');
   const [correctWord, setCorrectWord] = useState('');
+
+  // Array of words that can be selected for the game
   const wordsArray = ['apple', 'banana', 'orange', 'grape', 'watermelon', 'strawberry', 'lemon'];
 
+  // Function to get a random word from the wordsArray
   const getRandomWord = () => {
     const randomIndex = Math.floor(Math.random() * wordsArray.length);
     return wordsArray[randomIndex];
   };
 
+  // useEffect hook to set a random word as the correctWord on page load
   useEffect(() => {
-    // Set a random word as the correctWord on page load
     setCorrectWord(getRandomWord());
   }, []);
 
+  // Speak the word passed into this function using the Web Speech API
   const speakWord = (word) => {
     const speechSynthesis = window.speechSynthesis;
     const utterance = new SpeechSynthesisUtterance(word);
     speechSynthesis.speak(utterance);
   };
 
+  // Handle the spoken word and update the spokenWord state
   const handleSpokenWord = (word) => {
     const spokenLetter = word.toLowerCase();
     setSpokenWord((prevSpokenWord) => prevSpokenWord + spokenLetter);
   };
 
+  // Check if the spoken word matches the correct word
   const checkSpelling = () => {
     const checkSpokenWord = spokenWord.replace(/\s+/g, '').toLowerCase();
     const checkSelectedWord = correctWord.replace(/\s+/g, '').toLowerCase();
-    
+
+    // Check if an attempt was made to spell the word before comparing the spoken word/letters with the selected word
     if (!checkSpokenWord) {
       alert('You must spell the word first.');
     } else if (checkSpokenWord === checkSelectedWord.slice(0, checkSpokenWord.length)) {
@@ -41,20 +50,24 @@ const SpellingGame = () => {
     }
   };
 
+  // Clear the spoken letters state
   const clearSpokenLetters = () => {
     setSpokenWord('');
   };
 
+  // Function to handle when the user wants to get a new word
   const handleNewWord = () => {
     setSpokenWord('');
     setCorrectWord(getRandomWord());
   };
 
+  // content for the SpellingGame component
   return (
     <div className="spelling-game">
       <h1>Spelling Game</h1>
       <h2>Please have your microphone plugged in and ready!</h2>
       <p>Selected Word: "{correctWord}"</p>
+      {/* SpeechRecognitionComponent is a custom component to recognize spoken words */}
       <SpeechRecognitionComponent handleSpokenWord={handleSpokenWord} />
       <button onClick={() => speakWord(correctWord)}>Hear Word</button>
       <button onClick={clearSpokenLetters}>Clear Spoken Letters</button>
