9300d261ae6dcf6ab7b87c536d79af9bc3da9235
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-08-07 21:43:19 -0500

    added login/signup/navbar pages and coded a little.

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./App.css";
 import SpellingGame from "./components/SpellingGame";
 import PracticeLetter from "./components/practiceLetter";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
+    <div>
+      <Navbar />
+    </div>
       <div>
         <SpellingGame />
       </div>
